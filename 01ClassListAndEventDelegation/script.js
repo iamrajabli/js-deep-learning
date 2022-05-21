e2187d027074a529ff6cdd9250060a691b04676a
iamrajabli <iamrajabli@outlook.com>
2022-05-21 22:21:04 +0400

    File updated

--- 01ClassListAndEventDelegation/script.js
+++ 01ClassListAndEventDelegation/script.js
@@ -4,3 +4,4 @@ console.log(btns[0].classList); // show all classes
 console.log(btns[0].classList.length); // show all classes length
 console.log(btns[0].classList.item(0)); // show first class
 btns[0].classList.add('red'); // add class .red
+btns[0].classList.remove('blue'); // remove class .blue
