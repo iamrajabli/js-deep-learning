de302af3851e038d1fe8c6cbadf5368126617cd5
iamrajabli <iamrajabli@outlook.com>
2022-05-21 22:19:24 +0400

    File updated

--- 01ClassListAndEventDelegation/script.js
+++ 01ClassListAndEventDelegation/script.js
@@ -2,3 +2,4 @@ const btns = document.querySelectorAll('button');
 
 console.log(btns[0].classList); // show all classes
 console.log(btns[0].classList.length); // show all classes length
+console.log(btns[0].classList.item(0)); // show first class
