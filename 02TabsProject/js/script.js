90e3a1f7aa86fa031d5ea10e51d5d30a54e9963d
iamrajabli <iamrajabli@outlook.com>
2022-05-22 00:44:10 +0400

    Project [TabsProject] completed

--- 02TabsProject/js/script.js
+++ 02TabsProject/js/script.js
@@ -1,1 +1,41 @@
-console.log(1);
+window.addEventListener('DOMContentLoaded', () => {
+    const tabsParent = document.querySelector('.tabheader__items'),
+        tabs = document.querySelectorAll('.tabheader__item'),
+        tabsContent = document.querySelectorAll('.tabcontent');
+
+
+    function hideTabContent() {
+        tabsContent.forEach(item => {
+            item.classList.add('hide');
+            item.classList.remove('show');
+            item.classList.remove('fade');
+        });
+
+        tabs.forEach(item => {
+            item.classList.remove('tabheader__item_active');
+        })
+    }
+
+    function showTabContent(i = 0) {
+        tabsContent[i].classList.add('show');
+        tabsContent[i].classList.add('fade');
+        tabsContent[i].classList.remove('hide');
+        tabs[i].classList.add('tabheader__item_active')
+    }
+
+    hideTabContent();
+    showTabContent();
+
+    tabsParent.addEventListener('click', (event) => {
+        const target = event.target;
+        if (target && target.classList.contains('tabheader__item')) {
+            tabs.forEach((item, i) => {
+                if (target == item) {
+                    hideTabContent();
+                    showTabContent(i);
+                    item.classList.add('tabheader__item_active');
+                }
+            })
+        }
+    })
+})
