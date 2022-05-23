dcaac64d740d116ff5deba7ba91c80631ca46cab
iamrajabli <iamrajabli@outlook.com>
2022-05-23 16:40:32 +0400

    File updated

--- 04DocumentAndWindow/script.js
+++ 04DocumentAndWindow/script.js
@@ -1,13 +1,28 @@
 'use strict';
 
-const box = document.querySelector('.box');
+const box = document.querySelector('.box'),
+    btn = document.querySelector('button');
 
 // clientWidth => without border, scroll
 // const width = box.clientWidth; 
 // const height = box.clientHeight;
 
 // offsetWidth => with border, scroll
-const width = box.offsetWidth
-const height = box.offsetHeight;
+// const width = box.offsetWidth
+// const height = box.offsetHeight;
 
-console.log(width, height);
+// scrollWidth => with scroll
+const width = box.scrollWidth
+const height = box.scrollHeight;
+
+btn.addEventListener('click', () => {
+    console.log(box.scrollTop);
+});
+
+// get all sizes
+console.log(box.getBoundingClientRect());
+
+// get all css styles
+const styleBox = window.getComputedStyle(box);
+
+console.log(styleBox.display);
