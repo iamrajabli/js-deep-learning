f3a854724e60c318b52e78c1df32ea20b751506d
iamrajabli <iamrajabli@outlook.com>
2022-05-25 02:05:41 +0400

    File update

--- 09PromiseNotes/promise.js
+++ 09PromiseNotes/promise.js
@@ -1,17 +1,41 @@
 'use strict';
 
-console.log('Загруска данных...');
+// console.log('Загруска данных...');
 
-setTimeout(() => {
-    console.log('Подготовка данных...');
+// setTimeout(() => {
+//     console.log('Подготовка данных...');
 
-    const product = {
-        name: 'TV',
-        price: 2000
-    }
+//     const product = {
+//         name: 'TV',
+//         price: 2000
+//     }
+
+//     setTimeout(() => {
+//         product.status = 'order';
+//         console.log(product);
+//     }, 2000)
+// }, 2000)
+
+const req = new Promise((resolve, reject) => {
+    console.log('Загруска данных...');
+
+    setTimeout(() => {
+        console.log('Подготовка данных...');
+
+        const product = {
+            name: 'TV',
+            price: 2000
+        }
+
+        resolve(product);
+
+    }, 2000)
+});
+
+req.then(product => {
 
     setTimeout(() => {
         product.status = 'order';
         console.log(product);
     }, 2000)
-}, 2000)
+})
