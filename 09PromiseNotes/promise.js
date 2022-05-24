4d1cbc9df29e23430e091146a1314eec9ec2ea68
iamrajabli <iamrajabli@outlook.com>
2022-05-25 03:24:08 +0400

    File update

--- 09PromiseNotes/promise.js
+++ 09PromiseNotes/promise.js
@@ -41,35 +41,49 @@
 // })
 
 
-const req = new Promise((resolve, reject) => {
-    console.log('Загруска данных...');
+// const req = new Promise((resolve, reject) => {
+//     console.log('Загруска данных...');
 
-    setTimeout(() => {
-        console.log('Подготовка данных...');
+//     setTimeout(() => {
+//         console.log('Подготовка данных...');
 
-        const product = {
-            name: 'TV',
-            price: 2000
-        }
+//         const product = {
+//             name: 'TV',
+//             price: 2000
+//         }
 
-        // resolve(product);
-        reject();
+//         // resolve(product);
+//         reject();
 
-    }, 2000)
-});
+//     }, 2000)
+// });
 
-req.then(product => new Promise((resolve, reject) => {
-    setTimeout(() => {
-        product.status = 'order';
-        resolve(product);
-    }, 2000);
-}).then(product => {
-    product.modify = true;
-    return product;
-}).then(product => {
-    console.log(product);
-})).catch(() => {
-    console.error('Произошла ошибка');
-}).finally(() => {
-    console.log('Finally');
-})
+// req.then(product => new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         product.status = 'order';
+//         resolve(product);
+//     }, 2000);
+// }).then(product => {
+//     product.modify = true;
+//     return product;
+// }).then(product => {
+//     console.log(product);
+// })).catch(() => {
+//     console.error('Произошла ошибка');
+// }).finally(() => {
+//     console.log('Finally');
+// });
+
+
+const test = time => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => resolve(), time)
+    })
+}
+
+// test(1000).then(() => console.log('1000 ms'));
+// test(2000).then(() => console.log('2000 ms'));
+
+// Promise.all([test(1000), test(2000)]).then(() => console.log('ALL'))
+
+Promise.race([test(1000), test(2000)]).then(() => console.log('ALL'))
