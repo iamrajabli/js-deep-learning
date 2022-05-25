7781c94665b411e9a4fe8f12823ff1d265e21feb
iamrajabli <iamrajabli@outlook.com>
2022-05-25 15:52:39 +0400

    File update

--- 11ArrayIterationMethods/js/script.js
+++ 11ArrayIterationMethods/js/script.js
@@ -1,22 +1,8 @@
 'use strict';
 
-const inputAzn = document.querySelector('#azn'),
-    inputUsd = document.querySelector('#usd');
+const names = ['Ivan', 'Ann', 'Ksenia', 'Pragmatech'];
 
+// filter
+const shortNames = names.filter(name => name.length < 5);
 
-inputAzn.addEventListener('input', () => {
-    const request = new XMLHttpRequest();
-
-    request.open('GET', './js/current.json');
-    request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
-    request.send();
-
-    request.addEventListener('load', () => {
-        if (request.status == 200) {
-            const response = JSON.parse(request.response);
-            inputUsd.value = (inputAzn.value / response.current.usd).toFixed(2);
-        }
-    });
-
-
-})
+// console.log(shortNames);
