532977ce4e541df72af95bd98bb1c7a6ff66da53
iamrajabli <iamrajabli@outlook.com>
2022-05-25 16:02:53 +0400

    File update

--- 11ArrayIterationMethods/js/script.js
+++ 11ArrayIterationMethods/js/script.js
@@ -10,3 +10,8 @@ console.log('filter: ', shortNames); // filter:  [ 'Ivan', 'Ann' ]
 const answers = ['BakU', 'NeTname', 'pRagMa', 'Unibank'];
 const result = answers.map(item => item.toLowerCase());
 console.log('map: ', result); // map:  [ 'baku', 'netname', 'pragma', 'unibank' ]
+
+// some
+const inputs = [12, 'hikmat', 'khnuvd'];
+const inputsResult = inputs.some(input => typeof(input) === 'number');
+console.log('inputsResult:', inputsResult);
