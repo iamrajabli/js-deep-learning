d0eff7e3b5c66debbf13dcbad2fd2d69503268b4
iamrajabli <iamrajabli@outlook.com>
2022-05-27 17:28:08 +0400

    File update

--- 17Regex/script.js
+++ 17Regex/script.js
@@ -3,8 +3,13 @@
 // const reg = new RegExp();
 // /pattern/f
 
-const ans = prompt('Enter your name');
-const reg = /n/ig;
+// const ans = prompt('Enter your name');
+// const reg = /n/ig;
 
 // console.log(ans.search(reg));
-console.log(ans.match(reg));
+// console.log(ans.match(reg));
+
+const pass = prompt('enter password');
+
+// just "."
+console.log(pass.replace(/\./g, "*"));
