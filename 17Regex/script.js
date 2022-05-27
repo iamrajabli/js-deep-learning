c342c687099123023aa199dba0aaa262fc603474
iamrajabli <iamrajabli@outlook.com>
2022-05-27 17:21:07 +0400

    File update

--- 17Regex/script.js
+++ 17Regex/script.js
@@ -4,6 +4,7 @@
 // /pattern/f
 
 const ans = prompt('Enter your name');
-const reg = /n/i;
+const reg = /n/ig;
 
-console.log(ans.search(reg));
+// console.log(ans.search(reg));
+console.log(ans.match(reg));
