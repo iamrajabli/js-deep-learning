6ca3a38fb58d76dc1ab4d098e42c73b5bf8470d1
iamrajabli <iamrajabli@outlook.com>
2022-05-27 17:38:47 +0400

    File update

--- 17Regex/script.js
+++ 17Regex/script.js
@@ -9,10 +9,16 @@
 // console.log(ans.search(reg));
 // console.log(ans.match(reg));
 
-const pass = prompt('enter password');
+// const pass = prompt('enter password');
 
 // just "."
-console.log(pass.replace(/\./g, "*"));
+// console.log(pass.replace(/\./g, "*"));
 
 // all 
-console.log(pass.replace(/./g, "*"));
+// console.log(pass.replace(/./g, "*"));
+
+// let time = '12-15-18';
+// console.log(time.replace(/-/g, ':')); // 12:15:18
+
+// const ans = prompt('enter px');
+// console.log(ans.match(/\d/g));
